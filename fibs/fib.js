1db85c66abc3da9cbdf49cdef0c2b611416741dc
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-01-22 18:07:09 -0600

    Delete fibs directory

--- fibs/fib.js
+++
@@ -1,9 +0,0 @@
-function fibonacci(n) {
-    let a = 0, b = 1;
-    for (let i = 0; i < n; i++) {
-        [a, b] = [b, a + b];
-    }
-    return a;
-}
-
-console.log(fibonacci(10));
--- fibs/fib.rb
+++
@@ -1,9 +0,0 @@
-def fib(n)
-  a, b = 0, 1
-  n.times do
-    a, b = b, a + b
-  end
-  a
-end
-
-puts fib(10)
